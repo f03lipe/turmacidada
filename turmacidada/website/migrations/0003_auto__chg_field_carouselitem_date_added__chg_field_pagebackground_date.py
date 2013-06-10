81512cbcce80836cc69c64adb867f39ae1a82ff2
f03lipe <pires.a.felipe@gmail.com>
2013-06-10 18:49:39 -0300

    model updated

---
+++ turmacidada/website/migrations/0003_auto__chg_field_carouselitem_date_added__chg_field_pagebackground_date.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'CarouselItem.date_added'
+        db.alter_column(u'website_carouselitem', 'date_added', self.gf('django.db.models.fields.DateTimeField')(auto_now=True))
+
+        # Changing field 'PageBackground.date_added'
+        db.alter_column(u'website_pagebackground', 'date_added', self.gf('django.db.models.fields.DateField')(auto_now_add=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'CarouselItem.date_added'
+        db.alter_column(u'website_carouselitem', 'date_added', self.gf('django.db.models.fields.DateField')(auto_now=True))
+
+        # Changing field 'PageBackground.date_added'
+        db.alter_column(u'website_pagebackground', 'date_added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True))
+
+    models = {
+        u'website.carouselitem': {
+            'Meta': {'object_name': 'CarouselItem'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '500', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'is_published': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'link': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '80', 'blank': 'True'})
+        },
+        u'website.newsfeeditem': {
+            'Meta': {'object_name': 'NewsFeedItem'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '350'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'website.pagebackground': {
+            'Meta': {'object_name': 'PageBackground'},
+            'date_added': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'file': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'header_color': ('django.db.models.fields.CharField', [], {'default': "'dark'", 'max_length': '20'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        u'website.projectitem': {
+            'Meta': {'object_name': 'ProjectItem'},
+            'banner': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'birth_date': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '400'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'small_desc': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'website.teammember': {
+            'Meta': {'object_name': 'TeamMember'},
+            'age': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'avatar': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'bio': ('django.db.models.fields.TextField', [], {'max_length': '240'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'member_since': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2007, 1, 1, 0, 0)'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'website.treemanager': {
+            'Meta': {'object_name': 'TreeManager'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['website']
